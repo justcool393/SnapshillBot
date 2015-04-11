20045dbaf3f2847740a3827579c536d88295c0b6
SnapshillBot <wayhay54@gmail.com>
2015-04-11 13:44:34 -0700

    Remove semicolons and try to fix a bug

--- bot-new.py
+++ bot-new.py
@@ -1,150 +1,169 @@
-import logging, os, praw, re, random, time, urlparse, urllib2, sys;
+import logging
+import os
+import praw
+import re
+import random
+import time
+import urlparse
+import urllib2
+import sys
 
-INFO = "/r/SSBot";
-CONTACT = "/message/compose?to=\/r\/SnapshillBot";
-ARCHIVE_SELF = os.environ['ARCHIVE_SELF'] is "1";
-SUBMISSION_SCAN_COUNT = 10;
+INFO = "/r/SSBot"
+CONTACT = "/message/compose?to=\/r\/SnapshillBot"
+ARCHIVE_SELF = os.environ['ARCHIVE_SELF'] is "1"
+SUBMISSION_SCAN_COUNT = 10
+SUBREDDIT = "Buttcoin+Oppression+RedditCensorship+SSBot+TheBluePill+undelete"
 
-archived = [];
-user = os.environ['REDDIT_USER'];
+archived = []
+user = os.environ['REDDIT_USER']
+
 
 def main():
-    r = praw.Reddit("Creates snapshots using archive.today", domain="api.reddit.com");
-    r.login(user, os.environ['REDDIT_PASS']);
-    logging.info("Logged in and started post archiving.");
-    add_archived(r);
-    s = r.get_subreddit('Buttcoin+Oppression+RedditCensorship+SSBot+TheBluePill+undelete');
+    r = praw.Reddit("Snapshot Bot (/u/justcool393)", domain="api.reddit.com")
+    r.login(user, os.environ['REDDIT_PASS'])
+    logging.info("Logged in and started post archiving.")
+    add_archived(r)
+    s = r.get_subreddit(SUBREDDIT)
 
-    check_at = 3600;
-    last_checked = 0;
-    times_zero = 1;
+    check_at = 3600
+    last_checked = 0
+    times_zero = 1
 
-    arch = archive_submissions(r, s, 50, 90);
+    arch = archive_submissions(r, s, 50, 90)
     # Check the last 50 posts on startup
     while True:
         if time.time() - last_checked > check_at:
-            last_checked = time.time();
+            last_checked = time.time()
             if arch == 0:
-                pass;
+                times_zero += 1
             else:
-                logging.info("Archived " + str(arch) + " in the last " + str((check_at * times_zero) / 60) + " minutes");
-                arch = 0;
-                times_zero = 1;
+                logging.info("Last " + str((check_at * times_zero) / 60)
+                             + "min: " + str(arch))
 
-        arch += archive_submissions(r, s, SUBMISSION_SCAN_COUNT, 240);
+                arch = 0
+                times_zero = 1
+
+        arch += archive_submissions(r, s, SUBMISSION_SCAN_COUNT, 240)
+
 
 def add_archived(r):
     for c in r.user.get_comments(sort='new', limit=None):
-        pid = c.parent_id;
+        pid = c.parent_id
         if pid is None or pid in archived:
-            continue;
-        archived.append(pid);
+            continue
+        archived.append(pid)
+
 
 def archive_submissions(r, s, count, delay):
-    archived_posts = 0;
+    archived_posts = 0
 
     for submission in s.get_new(limit=count):
         if submission.id in archived:
-            continue;
+            continue
 
-        submission.replace_more_comments(limit=None, threshold=0);
+        submission.replace_more_comments(limit=None, threshold=0)
 
-        commented = check_commented(submission);
+        commented = check_commented(submission)
 
         if commented:
-            archived.append(submission.id);
-            continue;
+            archived.append(submission.id)
+            continue
 
         try:
             if archive_and_post(submission):
-                archived_posts += 1;
-                archived.append(submission.id);
+                archived_posts += 1
+                archived.append(submission.id)
         except UnicodeEncodeError:
-            logging.error("Unable to archive post (UnicodeEncodeError, Submission ID: " + submission.id + ")");
+            logging.error("Error (UEE): Submission ID: " + submission.id + ")")
 
-    time.sleep(delay);
-    return archived_posts;
+    time.sleep(delay)
+    return archived_posts
+
 
 def check_commented(s):
-    flat_comments = praw.helpers.flatten_tree(s.comments);
+    flat_comments = praw.helpers.flatten_tree(s.comments)
     for c in flat_comments:
         if c.author is None:
-            continue;
+            continue
         if c.author.name == user:
-            return True;
-    return False;
+            return True
+    return False
 
 
 def get_response(url, data):
-    res = urllib2.urlopen(fix_url(url), data);
-    return res.read();
+    res = urllib2.urlopen(fix_url(url), data)
+    return res.read()
 
 
 def get_redirected_url(data):
-    return re.findall('http[s]?://archive.today/[0-z]{1,6}', data)[0];
-    #return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data)[0];
+    return re.findall('http[s]?://archive.today/[0-z]{1,6}', data)[0]
 
 
 def archive_and_post(s):
     if s.is_self and not ARCHIVE_SELF:
-        return False;
-    arch_post = archive(s.url);
-    return post(s, arch_post);
+        return False
+    arch_post = archive(s.url)
+    return post(s, arch_post)
 
 
 def archive(url):
-    return get_redirected_url(get_response("https://archive.today/submit/", "url=" + url));
+    return get_redirected_url(get_response("https://archive.today/submit/",
+                                           "url='" + url + "'"))
 
 
-def post(s, archived):
+def post(s, archive_link):
     comment = """
 Automatically archived [here]({link}).
 
 *I am a bot. ([Info]({info}) | [Contact]({contact}))*
-""";
+"""
 
     try:
-        s.add_comment(comment.format(link=archived, info=INFO, contact=CONTACT));
-    except Exception as e:
-        logging.error("Error adding comment. (Submission ID: " + str(s.id) + ")");
-        logging.error(str(e));
-        return False;
-    return True;
+        s.add_comment(
+            comment.format(link=archive_link, info=INFO, contact=CONTACT))
+    except Exception as ex:
+        logging.error(
+            "Error adding comment. (Submission ID: " + str(s.id) + ")")
+        logging.error(str(ex))
+        return False
+    return True
 
 
 def urlEncodeNonAscii(b):
-    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b);
+    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
 
 
 def fix_url(iri):
-    parts = urlparse.urlparse(iri);
+    parts = urlparse.urlparse(iri)
     return urlparse.urlunparse(
-        part.encode('idna') if parti == 1 else urlEncodeNonAscii(part.encode('utf-8'))
+        part.encode('idna') if parti == 1 else urlEncodeNonAscii(
+            part.encode('utf-8'))
         for parti, part in enumerate(parts)
-    );
+    )
+
 
 def setup_logging():
-    root = logging.getLogger();
-    root.setLevel(logging.INFO);
-    logging.getLogger("requests").setLevel(logging.WARNING);
-    ch = logging.StreamHandler(sys.stdout);
-    ch.setLevel(logging.INFO);
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
 
-    root.addHandler(ch);
+    root.addHandler(ch)
+
 
 def log_crash(e):
-    logging.error("Error occurred in the bot; restarting in 15 seconds...");
-    logging.error("Details: " + str(e));
-    time.sleep(15);
-    sys.exit(1); # Signal to the host that we crashed
+    logging.error("Error occurred in the bot restarting in 15 seconds...")
+    logging.error("Details: " + str(e))
+    time.sleep(15)
+    sys.exit(1)  # Signal to the host that we crashed
 
 
 try:
-    setup_logging();
-    main();
+    setup_logging()
+    main()
 except (NameError, SyntaxError, AttributeError) as e:
-    logging.error("Syntatical error!");
-    logging.error(str(e));
-    time.sleep(86400); # Sleep for 1 day so we don't restart.
+    logging.error(str(e))
+    time.sleep(86400)  # Sleep for 1 day so we don't restart.
 except Exception as e:
-    log_crash(e);
+    log_crash(e)
