0c8b3563df4c0a7b7b1e7dbb0b93200194c52547
SnapshillBot <wayhay53@gmail.com>
2017-05-30 21:52:41 -0700

    Enable archiving for archive.is links.

--- snapshill.py
+++ snapshill.py
@@ -127,8 +127,8 @@ class ArchiveIsArchive(NameMixin):
 
     def __init__(self, url):
         self.url = url
-        # self.archived = self.archive()
-        self.archived = False
+        self.archived = self.archive()
+        # self.archived = False
         pairs = {"url": self.url, "run": 1}
         self.error_link = "https://archive.is/?" + urlencode(pairs)
 
