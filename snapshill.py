daa4b8d46786689c453e571d51da20f166eba415
justcool393 <justcool393@gmail.com>
2019-07-31 09:05:24 -0700

    Fix bug with language code links and other mentions

--- snapshill.py
+++ snapshill.py
@@ -81,9 +81,9 @@ def fix_url(url):
     :param url: URL to change.
     :return: Returns a fixed URL
     """
-    if url.startswith("r/") or url.startswith("u/"):
-        url = "http://www.reddit.com/" + url
-    return re.sub(REDDIT_PATTERN, "http://www.reddit.com", url)
+    if url.startswith("/r/") or url.startswith("/u/"):
+        url = "http://old.reddit.com/" + url
+    return re.sub(REDDIT_PATTERN, "http://old.reddit.com", url)
 
 
 def skip_url(url):
